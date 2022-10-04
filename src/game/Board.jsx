37ce2e4aa67a797c207ed7308d7dc773161b3fb6
leticia <tishcode@gmail.com>
2022-10-04 13:04:12 -0300

    update board

--- src/game/Board.jsx
+++ src/game/Board.jsx
@@ -2,43 +2,42 @@ import React, { useEffect, useState } from "react";
 import Result from "./Result";
 
 const Board = () => {
-  const [currentPlayer, setCurrentPlayer] = useState(1);
+  const [currentPlayer, setCurrentPlayer] = useState(null);
   const [winner, setWinner] = useState(null);
   const template = Array(3).fill(0);
   const [board, setBoard] = useState(Array(3).fill(template));
 
+  // called everytime the board changes
   useEffect(() => {
-    const checkWin = () => {
-      return checkRows(board) || checkColumns(board);
-      //checkDiagonals
-    };
-
-    const checkRows = (rows) => {
-      return rows.some((row) => row[0] && row.every((item) => item === row[0]));
-    };
-
-    const transpose = (matrix) => {
-      let [row] = matrix;
-      return row.map((value, column) => matrix.map((row) => row[column]));
-    };
-
-    const checkColumns = () => {
-      const transposed = transpose(board);
-      return checkRows(transposed);
-    };
-
-    const checkTie = () => {
-      return !board.some((row) => row.some((item) => item === 0));
-    };
-
-    const changePlayer = () => {
-      setCurrentPlayer(-currentPlayer);
-    };
-
-    if (checkWin()) setWinner(currentPlayer);
+    if (checkRows(board) || checkColumns(board)) setWinner(currentPlayer);
     else if (checkTie()) setWinner(0);
-    else changePlayer();
+    else setCurrentPlayer(-currentPlayer);
+
+    // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [board]);
+
+  // works as a ComponentDidMount
+  useEffect(() => {
+    setCurrentPlayer(1);
+  }, []);
+
+  const checkRows = (rows) => {
+    return rows.some((row) => row[0] && row.every((item) => item === row[0]));
+  };
+
+  const transpose = (matrix) => {
+    let [row] = matrix;
+    return row.map((value, column) => matrix.map((row) => row[column]));
+  };
+
+  const checkColumns = () => {
+    const transposed = transpose(board);
+    return checkRows(transposed);
+  };
+
+  const checkTie = () => {
+    return !board.some((row) => row.some((item) => item === 0));
+  };
 
   const clickButton = (row, col) => {
     if (board[row][col]) return;
@@ -65,10 +64,13 @@ const Board = () => {
     <>
       {winner === null ? (
         <>
-          <section>{`Player ${playerName(
-            currentPlayer
-          )}, it's your turn!`}</section>
-          <section id="board" className="flex items-center justify-center">
+          <section
+            id="board"
+            className="flex items-center justify-center flex-col h-full"
+          >
+            <div className="text-center my-4">{`Player ${playerName(
+              currentPlayer
+            )}, it's your turn!`}</div>
             <div className="border border-black w-36 h-36">
               {board.map((cols, row) => (
                 <div id={"row-" + row} key={row} className="flex h-1/3">
